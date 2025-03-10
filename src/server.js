d6a2775a5162279f81e5ee46f02f9909dbd10181
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-03-10 18:03:35 +0200

    add endpoints for openapi

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -26,7 +27,7 @@ export const startServer = async () => {
   app.use(cookieParser());
   app.use(
     cors({
-      origin: 'https://h2-o-flow-team4.vercel.app',
+      origin: ['http://127.0.0.1:3000', 'https://h2-o-flow-team4.vercel.app'],
       credentials: true,
     }),
   );
@@ -38,11 +39,14 @@ export const startServer = async () => {
   });
   app.use(logger);
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router);
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
-  app.use('/uploads', express.static(UPLOAD_DIR));
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
