88a0f9caae095914dc421a799c629275d561298b
Konstantin <stecenkok@gmail.com>
2025-03-14 03:05:30 +0200

    fix avatar

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import multer from 'multer';
 import { pinoHttp } from 'pino-http';
 import { UPLOAD_DIR } from './constants/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -17,33 +15,19 @@ const PORT = Number(getEnvVar('PORT'));
 
 export const startServer = async () => {
   const app = express();
-  const upload = multer();
-  app.use(upload.none());
+
   app.use(express.urlencoded({ extended: true }));
   app.use(
     express.json({ type: ['application/json', 'application/vnd.api+json'] }),
   );
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
-  // added 35 packages
-  const corsOptions = {
-    origin: [
-      'http://localhost:5173',
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
-      'https://h2-o-flow-team4.vercel.app',
-    ],
-    credentials: true,
-  };
-
-  // app.use(
-  //   cors({
-  //     origin: '*',
-  //   }),
-  // );
-
-  app.use(cors(corsOptions));
-  app.options('*', cors(corsOptions));
+  app.use(
+    cors({
+      origin: 'https://h2-o-flow-team4.vercel.app',
+      credentials: true,
+    }),
+  );
 
   const logger = pinoHttp({
     transport: {
@@ -52,22 +36,11 @@ export const startServer = async () => {
   });
   app.use(logger);
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
-
-  app.use((req, res, next) => {
-    res.setHeader(
-      'Content-Security-Policy',
-      "script-src 'self' 'nonce-abc123'",
-    );
-    next();
-  });
-
   app.use(router);
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
-
+  app.use('/uploads', express.static(UPLOAD_DIR));
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
