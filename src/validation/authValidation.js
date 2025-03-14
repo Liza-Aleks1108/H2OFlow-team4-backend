a6b9957a043a3120651649e4887b69e48f923dbc
Konstantin <stecenkok@gmail.com>
2025-03-14 22:44:25 +0200

    fix  validation

--- src/validation/authValidation.js
+++ src/validation/authValidation.js
@@ -13,7 +13,7 @@ export const validationUpdateUserSchema = Joi.object({
   name: Joi.string().min(2).max(12).optional(),
   email: Joi.string().email().optional(),
   gender: Joi.string().valid('woman', 'man').optional(),
-  weight: Joi.number().min(1).max(500).optional(),
+  weight: Joi.number().min(0).max(500).optional(),
   dailySportTime: Joi.number().min(0).max(24).optional(),
   dailyNorm: Joi.number().min(500).max(15000).optional(),
   avatarUrl: Joi.string().uri().optional(),
